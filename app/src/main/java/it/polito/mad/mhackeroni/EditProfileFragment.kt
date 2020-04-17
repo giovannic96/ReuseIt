3d42192ed79b37215be1308885b126909a6cee22
Marco Greco <s262624@studenti.polito.it>
2020-04-17 17:57:53 +0200

    Setup navigation for profile

--- app/src/main/java/it/polito/mad/mhackeroni/EditProfileFragment.kt
+++ app/src/main/java/it/polito/mad/mhackeroni/EditProfileFragment.kt
@@ -1,15 +1,18 @@
 package it.polito.mad.mhackeroni
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
 
 class EditProfileFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_edit_profile, container, false)
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/it/polito/mad/mhackeroni/ShowProfileFragment.kt
+++ app/src/main/java/it/polito/mad/mhackeroni/ShowProfileFragment.kt
@@ -1,20 +1,84 @@
 package it.polito.mad.mhackeroni
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_show_profile.*
+
 
 class ShowProfileFragment : Fragment(){
+    var profile: MutableLiveData<Profile> = MutableLiveData()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_show_profile, container, false)
+        val v = inflater.inflate(R.layout.fragment_show_profile, container, false)
+        setHasOptionsMenu(true)
+        return v
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val sharedPref:SharedPreferences = context!!.getSharedPreferences(getString(R.string.shared_pref), Context.MODE_PRIVATE)
 
-        // Customize UI
+        loadData(sharedPref)
+
+        profile.observe(this, androidx.lifecycle.Observer {
+            try {
+                imageProfile.setImageBitmap(profile.value?.image?.let {
+                    it1 -> ImageUtils.getBitmap(it1, context!!)
+                })
+            } catch (e: Exception) {
+                Snackbar.make(view, R.string.image_not_found, Snackbar.LENGTH_SHORT).show()
+            }
+
+            fullname.text = profile.value?.fullName ?: resources.getString(R.string.defaultFullName)
+            bio.text = profile.value?.bio ?: resources.getString(R.string.defaultNickname)
+            nickname.text = profile.value?.nickname ?: resources.getString(R.string.defaultNickname)
+            mail.text = profile.value?.email ?: resources.getString(R.string.defaultEmail)
+            phone_number.text = profile.value?.phoneNumber ?: resources.getString(R.string.defaultLocation)
+            location.text = profile.value?.location ?: resources.getString(R.string.defaultLocation)
+        })
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.main_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle menu item selection
+        return when (item.itemId) {
+            R.id.menu_edit -> {
+                editProfile()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun editProfile(){
+        bundleOf("profile" to profile.value?.let { Profile.toJSON(it).toString() })
+        fragmentManager?.beginTransaction()
+                ?.replace(R.id.fragment, EditProfileFragment())
+                ?.commit()
+    }
+
+    private fun saveData(s: SharedPreferences, p:Profile) {
+        with (s.edit()) {
+            putString(getString(it.polito.mad.mhackeroni.R.string.profile_sharedPref), Profile.toJSON(p).toString())
+            apply()
+        }
+    }
+
+    private fun loadData(s: SharedPreferences){
+        val JSONString : String? = s.getString(getString(R.string.profile_sharedPref), "")
+        profile.value = JSONString?.let { Profile.fromStringJSON(it) }
+    }
+
+
 }
